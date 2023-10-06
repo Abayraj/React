c08ab3578534b4e4b92400cf6140759bcd9d4fb2
Abayraj <abayunni5@gmail.com>
2023-10-06 14:04:39 +0530

    array's methods

--- React prerequisites  javascript/index.js
+++ React prerequisites  javascript/index.js
@@ -105,3 +105,114 @@ names of the object you are destructuring.
 
 const {name:namep1,age:agep1,isMarried} =  person1
 console.log(namep1)
+
+
+
+//spread operator using for make a copy of object to another object and 
+//here changes only a value to person4
+//it can work the same way in arrays also
+const person3 = {
+    name:"Rambo",
+    age:2,
+    isMarried:true,
+
+}
+const person4 = {...person3,isMarried:false}
+console.log(person3)
+console.log(person4)
+
+//we are taking dogs name and putting a copy to dogsneeded and adding a new value to it cooper
+const Dogs =['rubi','jubi','juno']
+
+const DogsNeeded = [...Dogs,"cooper"]
+
+
+//Next is three functions of javascript array
+/**
+.map()
+.filter()
+. reduce()
+
+ */
+
+
+let pets = ['rado','lado','sedo'];
+let anotherPets =['rocky','lucky'];
+
+//pets.push("mango") //Add new value to last
+//pets.pop() Delete the last value
+//pets.unshift("hello") Add new value to front
+//pets.shift()// Delete the first value
+
+
+//To join two arrays and to put a another variable
+// const allPets = pets.concat(anotherPets);
+// console.log(allPets)
+
+//splice
+//The first index which will be the changes will begin
+//The next index is optional how may elements need to be deleted
+// pets.splice(0,1,"Rado lado replaced")
+// console.log(pets)
+
+
+// pets.splice(1,2)//removes elements from first index and second
+// console.log(pets)
+
+
+//Slice returns a shallow copy of a portion of an array.
+// const slicedPets = pets.slice(0,2)
+// console.log(slicedPets)
+
+
+//splice
+console.log(pets,'==pets')
+pets.splice(0,1,"added first element")
+console.log(pets,"==replaced first element and added new one");
+
+const numbers = [10,20,30,40];
+
+
+/*
+map(): This method creates a new array by 
+applying a function to each element in the original array.
+*/
+
+// if did not put  brackets after => {} don't need to give return  
+
+numbers.map((current,index,Arrayfull)=>
+    console.log(current,index,Arrayfull)
+    
+)
+
+//forEach(): Executes a provided function once for each array element.
+const num = [1, 2, 3];
+num.forEach(currNum => console.log(currNum,"current numbers")); // Logs each number (1, 2, 3)
+
+//but if we give {} need to give return or it will not work sometimes
+// numbers.map((current,index,Arrayfull)=>{
+//    return console.log(current,index,Arrayfull)
+    
+// })
+
+const dobuleValue = numbers.map(current=>
+    current*2
+
+)
+
+console.log(dobuleValue,"dobule value")
+
+const filterValue = dobuleValue.filter(curr=>
+    curr%2===0)
+
+    console.log(filterValue," even values")
+
+
+//reduce
+
+const reducedValue = dobuleValue.reduce((acc,curr)=>
+    acc+=curr
+    
+    );
+
+    console.log(reducedValue,"total values of the array is");
